543232450f6a01436f89815eb83b24abf16674b4
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-01 12:00:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,12 +11,12 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species
-  COUNT(species) 
-  AS value_occurrence
-  FROM characters 
- GROUP BY species
- ORDER BY value_occurrence DESC
- LIMIT 1"
+   COUNT(species) 
+   AS value_occurrence
+   FROM characters 
+   GROUP BY species
+   ORDER BY value_occurrence DESC
+   LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
