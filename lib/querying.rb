833226298bdcc0470e90e868f741dee0fea07dd4
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-01 12:42:03 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  ORDER BY COUNT(series.title) AS value_occurrence DESC
+  ORDER BY COUNT(series.title)  DESC
    LIMIT 1"
   
 end
