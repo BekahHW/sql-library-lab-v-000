68bf8a6a14d0ad2e160efe0d7be86cb508160055
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-01 12:37:09 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,6 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title 
-  # , characters.species
   FROM series
   JOIN characters
   ON series.id = characters.series_id
