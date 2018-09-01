6455208a3f0f6af1d2396fb177e6c04e1be62457
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-01 12:22:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,12 +21,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  JOIN subgenres
+  INNER JOIN subgenres
   ON authors.id = subgenres.author_id"
-  
-FROM Cats
-INNER JOIN Owners
-ON Cats.owner_id = Owners.id;
 end
 
 def select_series_title_with_most_human_characters
